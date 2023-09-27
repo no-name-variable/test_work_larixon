5c70cea5d8297cb0a7aee340f393cef429fe38ac
yotamvladgmail.com <yotamvlad@gmail.com>
2023-09-27 14:07:54 +0300

    add static root directory

--- django_application/config/settings/base.py
+++ django_application/config/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,9 +138,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = os.getenv('STATIC_URL', '/api/static/')
+STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 cd django_application
 
+python manage.py collectstatic --noinput --clear
 python manage.py migrate --noinput
 uwsgi --ini config/server/uwsgi.ini --stats :3031 --stats-http
 
